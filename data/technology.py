86fe94a2d322c3355b7cb108b557e0ac9f4f2f71
BFourquin <benoit.fourquin@ynov.com>
2022-05-17 21:32:39 +0200

    Technology generator from excel

---
+++ data/technology.py
@@ -0,0 +1,30 @@
+
+
+technologies = {
+
+
+
+
+
+}
+
+tech_types = ['general theory',
+              'social', 'biology', 'electronic', 'energy'
+              'spaceship', 'weaponry', 'military doctrine',
+              'planetology', 'industry', '']
+
+
+def tech(internal_name, name, tech_type, description, tech_level, modifiers, price, difficulty):
+    return {'internal_name': None,
+            'name': None,
+            'tech_type': None,
+            'description': None,
+            'tech_level': None,
+            'difficulty': None,
+            'modifiers': None,
+            'prices': None,
+            'running_event': None,
+            'running_event_probability': None,
+            'end_event': None,
+            'end_event_probability': None,
+            }
---
+++ database/load_technologies.py
@@ -0,0 +1,41 @@
+
+import pylightxl as xl
+from database.db_connect import clients
+
+
+def load_technologies(excel_tech_path, sheet_tech_name, server_name, delete_actual_techs):
+
+    client = clients['TSS_'+server_name]
+    db = client['technology']
+    if delete_actual_techs:
+        db.remove({})
+
+
+    with open(excel_tech_path, 'rb') as f:
+        xldb = xl.readxl(f, ws='Techs')
+
+        for row in xldb.ws(ws=sheet_tech_name).rows:
+
+            if row[0] and not row[0][0] == '#':
+
+                # Manual end of file
+                if row[0] == 'END_OF_FILE':
+                    break
+
+                # Header line
+                if row[0] == 'internal_name':
+                    titles = row
+
+                tech = {}
+                for i in range(len(titles)):
+                    tech[titles[i]] = row[i] if row[i] else None
+
+                print(row)
+                db.insert_one(tech)
+
+
+load_technologies(excel_tech_path='C:\\Users\\Benoit\\Desktop\\Thousand Suns Saga\\TSS.xlsx',
+                  sheet_tech_name='Techs',
+
+                  server_name='Alpha',
+                  delete_actual_techs=True)
