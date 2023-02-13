60b91cd3af677b8dec39645b3517d96bd805c424
BFourquin <benoit.fourquin@ynov.com>
2023-02-13 16:54:32 +0100

    Positions infos for coordinates in DB

--- geography/mg_coordinate.py
+++ geography/mg_coordinate.py
@@ -2,13 +2,27 @@
 from data import coordinates, statistics
 
 
+def info_from_seed(seed):
+    seed = str(seed)
+    sector_id = int(seed[0:10])
+    system_id = int(seed[0:16])
+    sys_y = int(seed[10:13])
+    sys_x = int(seed[13:16])
+    sys_coordinate = int(seed[16:18])
+    return sector_id, system_id, sys_y, sys_x, sys_coordinate
+
+
 def create_coordinate(server, seed, coordinate_type, subtype):
 
-    coordinate = {
-        '_id': seed,
-        'type': coordinate_type,
-        'subtype': subtype
-    }
+    coordinate = {'_id': seed,
+                  'type': coordinate_type,
+                  'subtype': subtype,
+                  'sector_id': info_from_seed(seed)[0],
+                  'system_id': info_from_seed(seed)[1],
+                  'sys_y': info_from_seed(seed)[2],
+                  'sys_x': info_from_seed(seed)[3],
+                  'sys_coordinate': info_from_seed(seed)[4],
+                  }
 
     coordinates.set_coordinate(server, coordinate)
 
