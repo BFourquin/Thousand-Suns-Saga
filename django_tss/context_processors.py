5c511060e4d725a3154a1ef72f78d8c38b19a4ed
BFourquin <benoit.fourquin@ynov.com>
2024-06-01 23:22:48 +0200

    Redirection fix

--- django_tss/context_processors.py
+++ django_tss/context_processors.py
@@ -5,9 +5,6 @@ from data import server_details
 from data.user import get_user_by_name
 
 
-
-
-
 def servers_details(request):
 
     game_servers = server_details.get_servers_names(open_servers=True, playable_servers=True, test_servers=False, old_servers=False, admin_visibility=True)
@@ -29,7 +26,6 @@ def localization(request):
 
     if request.user:
         user_account = get_user_by_name(str(request.user))
-        print(dict(request.session))
         if user_account and 'language' in user_account:
             translation.activate(user_account['language'])
     return {}
